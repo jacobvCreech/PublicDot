bef28e07aa774a1323cc2f21a7e56d83a3ec0a7c
jacobvCreech <gaming.bradda01@gmail.com>
2024-04-26 17:55:20 +0930

    Refactored some code

--- app/page.tsx
+++ app/page.tsx
@@ -866,3 +866,4 @@ export default function Home() {
 // Update on 2024-04-24 14:55:06: Fix minor bug - 2921
 // Update on 2024-04-26 20:13:44: Improved performance - 7228
 // Update on 2024-04-26 20:29:02: Updated README - 2465
+// Update on 2024-04-26 17:55:20: Refactored some code - 3550
