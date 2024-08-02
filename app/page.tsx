eb70a234677dd8e6b2ba93c32ad6b01f9f600fb8
jacobvCreech <gaming.bradda01@gmail.com>
2024-08-02 20:12:58 +0930

    Small UI tweak

--- app/page.tsx
+++ app/page.tsx
@@ -993,3 +993,4 @@ export default function Home() {
 // Update on 2024-07-31 12:02:04: Refactored some code - 3925
 // Update on 2024-07-31 11:23:43: Minor documentation update - 5830
 // Update on 2024-08-01 09:05:15: Fix minor bug - 4517
+// Update on 2024-08-02 20:12:58: Small UI tweak - 1587
