d5de972017f69fa2612db94d11722987d5d8729e
jacobvCreech <gaming.bradda01@gmail.com>
2024-09-19 09:29:12 +0930

    Fix minor bug

--- app/page.tsx
+++ app/page.tsx
@@ -1058,3 +1058,4 @@ export default function Home() {
 // Update on 2024-09-17 08:07:45: Refactored some code - 9843
 // Update on 2024-09-17 20:57:23: Minor documentation update - 9671
 // Update on 2024-09-19 15:19:45: Updated README - 9290
+// Update on 2024-09-19 09:29:12: Fix minor bug - 4767
