b52c82e681e867a4d037cd7553406475dc2460ce
DotAiDev <gaming.bradda01@gmail.com>
2024-10-10 17:25:09 +1030

    Fix minor bug

--- app/page.tsx
+++ app/page.tsx
@@ -516,3 +516,4 @@ export default function Home() {
 // Update on 2024-10-09 19:10:32: Refactored some code - 5044
 // Update on 2024-10-09 11:30:33: Refactored some code - 2430
 // Update on 2024-10-10 08:58:03: Added new feature - 8217
+// Update on 2024-10-10 17:25:09: Fix minor bug - 7926
