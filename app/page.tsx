d8f289b008db12e07f8bc8cf201356bfed10feec
jacobvCreech <gaming.bradda01@gmail.com>
2024-10-18 16:29:17 +1030

    Code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1112,3 +1112,4 @@ export default function Home() {
 // Update on 2024-10-17 21:32:02: Refactored some code - 4897
 // Update on 2024-10-17 22:00:11: Fix minor bug - 7283
 // Update on 2024-10-18 21:40:00: Updated README - 7861
+// Update on 2024-10-18 16:29:17: Code cleanup - 2463
