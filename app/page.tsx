748f040c8fc91f863206c08a8ba401d4c2efabbc
DotAiDev <gaming.bradda01@gmail.com>
2024-11-02 08:10:05 +1030

    Minor documentation update

--- app/page.tsx
+++ app/page.tsx
@@ -196,3 +196,4 @@ export default function Home() {
 // Update on 2024-11-01 15:45:20: Small UI tweak - 9032
 // Update on 2024-11-01 21:31:28: Updated README - 2998
 // Update on 2024-11-01 09:12:45: Refactored some code - 3035
+// Update on 2024-11-02 08:10:05: Minor documentation update - 3321
