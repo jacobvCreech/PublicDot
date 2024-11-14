a0e5ab83be6538db1145c3e3f072b2a74e4548c6
DotAiDev <gaming.bradda01@gmail.com>
2024-11-14 13:18:40 +1030

    Small UI tweak

--- app/page.tsx
+++ app/page.tsx
@@ -218,3 +218,4 @@ export default function Home() {
 // Update on 2024-11-12 12:33:14: Updated dependencies - 9691
 // Update on 2024-11-13 16:13:32: Code cleanup - 6820
 // Update on 2024-11-14 17:58:30: Minor documentation update - 5172
+// Update on 2024-11-14 13:18:40: Small UI tweak - 5565
