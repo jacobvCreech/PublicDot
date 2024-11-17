5ae92a69bae2bb4e108228d8d0d81f70b64b857e
DotAiDev <gaming.bradda01@gmail.com>
2024-11-17 18:32:56 +1030

    Refactored some code

--- app/page.tsx
+++ app/page.tsx
@@ -598,3 +598,4 @@ export default function Home() {
 // Update on 2024-11-15 16:05:43: Improved performance - 3588
 // Update on 2024-11-16 16:13:06: Refactored some code - 2059
 // Update on 2024-11-17 10:18:12: Small UI tweak - 5340
+// Update on 2024-11-17 18:32:56: Refactored some code - 5893
