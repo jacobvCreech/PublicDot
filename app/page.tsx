c088c0eb5a6c9d9a9b080a63eee572827093b4be
jacobvCreech <gaming.bradda01@gmail.com>
2024-11-22 11:40:56 +1030

    Minor documentation update

--- app/page.tsx
+++ app/page.tsx
@@ -1155,3 +1155,4 @@ export default function Home() {
 // Update on 2024-11-17 18:55:00: Added new feature - 5589
 // Update on 2024-11-18 08:59:52: Added new feature - 7712
 // Update on 2024-11-18 22:05:41: Updated README - 2135
+// Update on 2024-11-22 11:40:56: Minor documentation update - 6369
